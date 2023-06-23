1fd789aeb8c0dba91408f4a6977db8e40b04a02d
Your Name <rghzaiel@ggn-group.com>
2023-06-23 16:02:17 +0100

    fix image picker ios error

--- components/places/ImagePicker.js
+++ components/places/ImagePicker.js
@@ -1,14 +1,50 @@
-import { Button, View } from "react-native";
-import { launchImageLibraryAsync } from "expo-image-picker";
+import { Button, View, Alert } from "react-native";
+import {
+  launchImageLibraryAsync,
+  useCameraPermissions,
+  PermissionStatus,
+} from "expo-image-picker";
 
 function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] =
+    useCameraPermissions();
+
+  async function verifyPermissions() {
+    if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted;
+    }
+
+    if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        "Insufficient Permissions!",
+        "You need to grant camera permissions to use this app."
+      );
+      return false;
+    }
+
+    return true;
+  }
+
   async function takeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+    /*  const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5,
+    }); */
     const image = await launchImageLibraryAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
   }
+
   return (
     <View>
       <View></View>
