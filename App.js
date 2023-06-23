e496012fdc5717865206d9a26b191f9a1e1ec475
Your Name <rghzaiel@ggn-group.com>
2023-06-23 11:24:01 +0100

    design header

--- App.js
+++ App.js
@@ -2,17 +2,42 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { StatusBar } from "expo-status-bar";
 import AllPlaces from "./screens/AllPlaces";
-
+import AddPlace from "./screens/AddPlace";
+import IconButton from "./components/UI/IconButton";
+import { Colors } from "./constants/colors";
 
 const Stack = createStackNavigator();
 export default function App() {
   return (
     <>
-      <StatusBar />
+      <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="addPlace" component={Home} />
-          <Stack.Screen name="places" component={AllPlaces} />
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
+          <Stack.Screen
+            name="AllPlaces"
+            component={AllPlaces}
+            options={({ navigation }) => ({
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  color={tintColor}
+                  size={24}
+                  onPress={() => navigation.navigate("AddPlace")}
+                />
+              ),
+            })}
+          />
+          <Stack.Screen
+            name="AddPlace"
+            component={AddPlace}
+            options={{ title: "Add a new Place" }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- components/places/PlacesList.js
+++ components/places/PlacesList.js
@@ -1,5 +1,6 @@
 import { View, Text, FlatList, StyleSheet } from "react-native";
 import PlaceItem from "./PlaceItem";
+import { Colors } from "../../constants/colors";
 
 function PlacesList({ places }) {
   if (!places || places.length === 0) {
@@ -29,6 +30,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   fallbackText: {
-    fontSize: 24,
+    fontSize: 16,
+    color: Colors.primary200,
   },
 });
