d12a751050cecf0c743d2a5f84f460b2617cad0e
Your Name <rghzaiel@ggn-group.com>
2023-06-26 11:45:09 +0100

    add amp interaction

--- screens/Map.js
+++ screens/Map.js
@@ -1,14 +1,39 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
-import MapView, { Maker } from "react-native-maps";
+import MapView, { Marker } from "react-native-maps";
 
 function Map() {
+  const [selectedLocation, setSelectedLocation] = useState();
   const region = {
     latitude: 37.78,
     longitude: -122.43,
     latitudeDelta: 0.0922,
     longitudeDelta: 0.0421,
   };
-  return <MapView style={styles.map} initialRegion={region}></MapView>;
+
+  function selectLocationHandler(event) {
+    const lat = event.nativeEvent.coordinate.latitude;
+    const lng = event.nativeEvent.coordinate.longitude;
+
+    setSelectedLocation({ lat: lat, lng: lng });
+  }
+  return (
+    <MapView
+      style={styles.map}
+      initialRegion={region}
+      onPress={selectLocationHandler}
+    >
+      {selectedLocation && (
+        <Marker
+          title="Picked Location"
+          coordinate={{
+            latitude: selectedLocation.lat,
+            longitude: selectedLocation.lng,
+          }}
+        />
+      )}
+    </MapView>
+  );
 }
 
 export default Map;
